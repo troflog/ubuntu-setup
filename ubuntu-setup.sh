5c103c9b606beb8a5a4f8ababc97d11792ed29c7
Trond B. Fløgstad <troflog@gmail.com>
2018-09-14 21:10:00 +0200

    Small change

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -37,7 +37,8 @@ silversearcher-ag virtualbox
 
 #----Vscode---
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/tr
+usted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get update
 sudo apt-get -y install code
