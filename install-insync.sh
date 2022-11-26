44540e5e08a200e8a135e94571d00f69b05d1f12
trond flogstad <troflog@gmail.com>
2022-11-26 18:05:30 +0100

    Delted files

--- install-insync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Insync
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
-echo "deb http://apt.insynchq.com/ubuntu xenial non-free contrib" | sudo tee /etc/apt/sources.list.d/insync.list
-sudo apt-get update
-sudo apt-get --yes --force-yes install insync
