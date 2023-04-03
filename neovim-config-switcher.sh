06c5dd4b53b684a632f090f2905701a10c08d467
troflog <troflog@gmail.com>
2023-04-03 12:53:25 +0200

    Create neovim-config-switcher.sh

---
+++ neovim-config-switcher.sh
@@ -0,0 +1,15 @@
+alias nvim-chad="NVIM_APPNAME=nvim-chad nvim"
+
+nvims() {
+  items=("default" "NvChad")
+  config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
+  if [[ -z $config ]]; then
+    echo "Nothing selected"
+    return 0
+  elif [[ $config == "default" ]]; then
+    config=""
+  fi
+  NVIM_APPNAME=$config nvim $@
+}
+
+bindkey -s ^a "nvims\n"
